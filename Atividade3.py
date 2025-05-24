d63f8edb27ba21bddc34a923457735448bf72a08
Nicolas Gonçalves <61568561+Yoriih@users.noreply.github.com>
2025-05-24 16:30:52 -0300

    Delete Atividade3.py

--- Atividade3.py
+++
@@ -1,6 +0,0 @@
-# Exercício 3 - Caixa eletrônico
-# Descrição:
-# Simule o funcionamento de um caixa eletrônico que:
-# Peça o valor a ser sacado (em reais).
-# Enquanto o valor informado for maior que o saldo disponível, informe que não é possível sacar e peça novamente.
-# Quando for possível, calcule e exiba a quantidade mínima de notas necessárias para o saque, considerando notas de 100, 50, 20, 10, 5 e 2 reais.
